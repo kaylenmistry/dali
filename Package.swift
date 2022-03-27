876db787d1bd39e1f058a20c6bad411d02bb544a
kaylenmistry <kaylenmistry@gmail.com>
2022-03-27 20:11:25 +0000

    Release v0.0.18

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Dali",
-            url: "https://github.com/kaylenmistry/dali/releases/download/v0.0.17/Dali.xcframework.zip", checksum: ""
+            url: "https://github.com/kaylenmistry/dali/releases/download/v0.0.18/Dali.xcframework.zip", checksum: ""
         )
     ]
 )
